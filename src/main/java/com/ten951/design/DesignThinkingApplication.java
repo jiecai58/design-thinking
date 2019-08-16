b5a28c0b17953d0dd5c2292e69023618dbc310dc
wangyongtian <ten951@qq.com>
2019-08-16 17:45:03 +0800

    first commit

---
+++ src/main/java/com/ten951/design/DesignThinkingApplication.java
@@ -0,0 +1,13 @@
+package com.ten951.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignThinkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignThinkingApplication.class, args);
+    }
+
+}
